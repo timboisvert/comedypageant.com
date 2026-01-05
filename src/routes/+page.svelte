e4326a481f267a91ffeb70a3e5e2197da3c9d271
Tim Boisvert <boisvert@gmail.com>
2026-01-05 10:58:37 -0600

    New Comedy Pageant / short form improv workshop site

---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<section class="split">
+    <div class="left">
+        <img
+            src="/images/comedypageant.png"
+            alt="Comedy Pageant poster"
+            class="poster"
+        />
+        <div class="info">
+            <p>
+                Who will be Pageant Bitch? Find out at Chicago's only improv
+                show!
+            </p>
+
+            <p>
+                Watch comedians compete in a short-form improv showdown that's
+                also a beauty pageant!
+            </p>
+
+            <p>
+                A rotating cast (no two shows are the same!) featuring: Dan
+                Feltey, Colleen Grogan, Quinn Hatch, Emmanuel J Ross Hartway,
+                Katie Rae Horn, Ben House, Trevor D. Kelley, Janelle Kloth, Zach
+                Masso, Cassie McGrath, Pasquale-Monk, Thom Murray, Declan Parker
+                Rhodes, Carlos Rivera, Clay Smith, Jp Winders, Andie Wonnacott
+            </p>
+
+            <p>
+                <a
+                    href="https://www.tickettailor.com/events/cocorunseverything/1878177?ref=website"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="buy"><span class="label">Buy Tickets</span></a
+                >
+            </p>
+        </div>
+    </div>
+
+    <div class="and">AND</div>
+
+    <div class="right">
+        <img
+            src="/images/short-form-improv-workshop.png"
+            alt="Short-form improv workshop poster"
+            class="poster"
+        />
+    </div>
+</section>
+
+<div class="band">
+    <a
+        href="https://www.cocorunseverything.com"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        <img
+            src="/images/coco-runs-everything-production-logo.png"
+            alt="Coco Runs Everything logo"
+        />
+    </a>
+    <p class="band-copyright">
+        © 2026 Coco Runs Everything LLC. All rights reserved.
+    </p>
+</div>
